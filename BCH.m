b475709c18a138832555dc3c81033b4baef12fbd
Gmaniko <niko220302@gmail.com>
2023-03-01 17:01:59 +0100

    Update BCH.m

--- BCH.m
+++ BCH.m
@@ -1,5 +1,5 @@
 %variables
-n = 256; %number of bits in codeword
+%n = 256; %number of bits in codeword
 
 %encode codeword
 %c = zeros(1,255);
@@ -34,6 +34,22 @@ for i = length(r)-1:-1:1
     S1 = bitxor(S1, [zeros(1,length(S1)-1) r(i)]);
 end
 
+
+alpha = zeros(7,3);
+a = [0 0 1];
+for i = 1:7
+    alpha(i,:) = a;
+    u = a(1);
+    a = [a(2:end) 0];
+    if u == 1
+        a = bitxor(a,P3(2:end));
+    end
+end
+
+alphainv = zeros(7,1);
+for i = 1:7
+    alphainv(polyval(alpha(i,:),2)) = i-1;
+end
 
 
 
