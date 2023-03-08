63f3ddbbdb66ebfd9b0c157f546cece42d4d60f2
Gmaniko <niko220302@gmail.com>
2023-03-08 16:10:07 +0100

    update

--- BCH.m
+++ BCH.m
@@ -3,8 +3,8 @@
 
 %encode codeword
 %c = zeros(1,255);
-c = [1 0 0 0 0 0 1];
-%c = [1 0 1 1 1 1 0];
+%c = [0 0 0 0 0 0 0];
+c = [1 0 1 1 0 1 0];
 %add noise to received bits
 p = 0.00;
 n = rand(1,length(c)) < p;
@@ -17,6 +17,7 @@ G = [1,0,1,1,0,1,1,1,1,0,1,1,0,0,0,1,1];
 S1 = [zeros(1,2) r(end)];
 S3 = [zeros(1,2) r(end)];
 %S = [zeros(1,2) r(end); zeros(1,2) r(end)];
+%Calculate  syndromes S1 and S3
 for i = length(r)-1:-1:1
     %S = mod(2*S + r(i),);
     for j = 1:3
@@ -36,7 +37,7 @@ for i = length(r)-1:-1:1
     S1 = bitxor(S1, [zeros(1,length(S1)-1) r(i)]);
 end
 
-
+%Calculate table that returns polynomial given alpha power + 1
 alpha = zeros(7,3);
 a = [0 0 1];
 for i = 1:7
@@ -48,19 +49,23 @@ for i = 1:7
     end
 end
 
+%Calculate table that returns alphapower given polynomial as integer
 alphainv = zeros(7,1);
 for i = 1:7
     alphainv(polyval(alpha(i,:),2)) = i-1;
 end
 
+%S1 to the third power in terms of alpha
 s1pow3 = mod(alphainv(polyval(S1,2),:)*3,2^(length(P)-1) - 1)
+%S3 in terms of alpha
 s3pow1 = alphainv(polyval(S3,2),:)
-
+%s1pow3 divided by s3pow1 in terms of alpha
 s1pow3s3 = mod(s3pow1 - s1pow3,2^(length(P)-1) - 1)
+%add 1 in terms of polynomials
 rat = alphainv(polyval(bitxor(alpha(s1pow3s3+1,:), [0 0 1]),2))
 
 
-
+%find y = x/S1
 y = [];
 for i = 1:6
     temp1 = bitxor(alpha(mod(2*i,2^(length(P)-1) - 1)+1,:),alpha(i+1,:));
@@ -69,5 +74,5 @@ for i = 1:6
         y = [y i];
     end
 end
-
+% x = y * S1
 x = mod(y+alphainv(polyval(S1,2),:),2^(length(P)-1) - 1);
