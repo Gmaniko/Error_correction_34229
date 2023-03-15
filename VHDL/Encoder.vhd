9301f8c68d832f484b9026e72896a481eb86780f
Gmaniko <niko220302@gmail.com>
2023-03-15 14:14:25 +0100

    encoder

--- VHDL/Encoder.vhd
+++ VHDL/Encoder.vhd
@@ -15,19 +15,18 @@ architecture encoder_arch of encoder is
 
 	constant G : std_logic_vector(3 downto 0) := "1011";
 	
-	signal R : std_logic_vector(6 downto 0);
+	signal R   : std_logic_vector(6 downto 0);
+	signal idx : integer range 0 to 6;
 
 	begin
 	
 	bindiv : process
 		begin
-			for i in 0 to R'length-1 loop
-				if To_integer(unsigned(G)) > To_integer(unsigned(R))
-					exit;
+			while To_integer(unsigned(G)) < To_integer(unsigned(R)) loop
+				if R(idx)='1' then
+					R((idx+G'length -1) downto idx) <= R((idx+G'length -1) downto idx) xor G;
 				end if;
-				if R(i)='1' then
-					R(i)(i to (i+G'length -1)) <= R(i)(i to (i+G'length -1)) xor G;
-				end if;
+				idx <= idx + 1;
 			end loop;
 	
 	end process;
