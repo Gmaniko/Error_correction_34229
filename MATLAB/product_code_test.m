d9ff2a122962b839beb16f746480067e876b0a13
Gmaniko <niko220302@gmail.com>
2023-03-16 03:41:26 +0100

    Update product_code_test.m

--- MATLAB/product_code_test.m
+++ MATLAB/product_code_test.m
@@ -6,32 +6,39 @@ rng('default')
 X = randi([0 1],K);
 
 %Encode
-product_code_enc(X, N, K);
+C = product_code_enc(X, N, K);
 
 %Add noise
 %rng('shuffle')
 p = 0.0085:0.0005:0.0125;
-BER = zeros(1,length(p));
+iterations = 1:2;
+BER = zeros(length(iterations),length(p));
 
-
-for i = 1:length(p)-4
-    noise = rand(N) < p(i);
-    %fprintf('Number of errors: %d\n', sum(noise,'all'))
-    R = bitxor(C,noise);
-    
-    
-    dec_message = product_code_dec(R,1, N);
-    
-    
-    if isequal(dec_message,C)
-        disp('Succesfully decoded.')
-    else
-        disp('Could not decode (too many errors).')
+for i = iterations
+    for j = 1:length(p)
+        noise = rand(N) < p(j);
+        %fprintf('Number of errors: %d\n', sum(noise,'all'))
+        R = bitxor(C,noise);
+        
+        
+        dec_message = product_code_dec(R, i, N);
+        
+        
+%         if isequal(dec_message,C)
+%             disp('Succesfully decoded.')
+%         else
+%             disp('Could not decode (too many errors).')
+%         end
+        
+        BER(i,j) = sum(bitxor(dec_message,C),'all')/(255^2);
     end
-    
-    BER(i) = sum(bitxor(dec_message,C),'all')/(255^2);
+    disp('iteration done.')
 end
-
 figure
-semilogy(p,BER)
+semilogy(p,BER(1,:))
+hold on
+for i = 2:10
+    semilogy(p,BER(i,:))
+end
+hold off
 ylim([10^(-10) 10^(-2)])
