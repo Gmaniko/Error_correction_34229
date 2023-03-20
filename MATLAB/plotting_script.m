2c2a2faba806e645a0e9f1bd6004eaafb6131b37
Gmaniko <niko220302@gmail.com>
2023-03-20 19:04:08 +0100

    update

--- MATLAB/plotting_script.m
+++ MATLAB/plotting_script.m
@@ -1,5 +1,5 @@
 figure
-semilogy(p,BER(1,:), 'DisplayName',sprintf('%d iteration',1), ...
+semilogy(p, channel, 'DisplayName',sprintf('Channel'), ...
     'Marker','.','LineWidth',1.5, 'MarkerSize',15)
 hold on
 xlabel('p')
@@ -8,7 +8,9 @@ ylabel('BER')
 
 xlim([0.008 0.013])
 ylim([10^(-10) 10^(-1)])
-for i = 2:iterations(end)
+semilogy(p,BER(1,:), 'DisplayName',sprintf('%d iteration',1), ...
+    'Marker','.','LineWidth',1.5, 'MarkerSize',15)
+for i = 2:iter(end)
     semilogy(p,BER(i,:), 'DisplayName',sprintf('%d iterations',i), ...
         'Marker','.','LineWidth',1.5, 'MarkerSize',15)
 end
--- MATLAB/product_code_test.m
+++ MATLAB/product_code_test.m
@@ -10,21 +10,24 @@ C = product_code_enc(X, N, K);
 
 %Add noise
 %rng('shuffle')
-p = linspace(0.00847,0.01257,12);
-iterations = 1:10;
-BER = zeros(length(iterations), length(p));
-
+%p = linspace(0.00847,0.01257,12);
+p = [0.00847 0.00879 0.00912 0.00946 0.00982 0.01019 0.01052 0.01095 0.01131 0.01175 0.01209 0.01258];
+iter = 1:10;
+BER = zeros(length(iter), length(p));
+channel = zeros(1,length(p));
 
 for j = 1:length(p)
     noise = rand(N) < p(j);
     R = bitxor(C,noise);
+    channel(j) = sum(noise,'all')/(255^2);
     dec_message = R;
-    for i = iterations
+    for i = iter
         %fprintf('Number of errors: %d\n', sum(noise,'all'))
         dec_message = product_code_dec(dec_message, N);
         BER(i,j) = sum(bitxor(dec_message,C),'all')/(255^2);
     end
     fprintf('error probability: %f\n',p(j))
 end
+
 
 plotting_script
