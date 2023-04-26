984cfed13ed7003d26a54d95f94efcb40f9dfed6
Mads <mads@zoie.dk>
2023-04-26 14:58:15 +0200

    Update AlphaToVHDL.m

--- AlphaToVHDL.m
+++ AlphaToVHDL.m
@@ -1,4 +1,5 @@
 
+
 fileID = fopen('VHDL.txt','w');
 s = "type ROM_type is array (0 to 254) of std_logic_vector(7 downto 0);\n";
 fprintf(fileID,s);
@@ -15,6 +16,20 @@ for i = 1:255
 end
 s=");";
 fprintf(fileID,s);
+
+s="\n\n\n";
+fprintf(fileID,s);
+s = "type ROM_type2 is array (0 to 254) of integer;\n";
+fprintf(fileID,s);
+s = "constant ROM_alphaInv : ROM_type2 := ( "
+fprintf(fileID,s);
+for i = 1:255
+     s = ""+ (i-1) + ' => ' + alphainv(i) + ',\n';
+     fprintf(fileID,s);
+end
+s=");";
+fprintf(fileID,s);
+
 fclose(fileID);
 
 %Remember to remove last comma , from VHDL    error -> ,);
