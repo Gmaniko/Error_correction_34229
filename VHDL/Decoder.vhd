d84994acdb1d81398929282982e29cca2c8e6cb6
Mads <mads@zoie.dk>
2023-04-26 09:03:42 +0200

    Update Decoder.vhd

--- VHDL/Decoder.vhd
+++ VHDL/Decoder.vhd
@@ -10,8 +10,16 @@ architecture Decoder_arch of Decoder is
 
 signal test : std_logic_vector(254 downto 0);
 signal S1result : std_logic_vector(7 downto 0);
+signal S3result : std_logic_vector(7 downto 0);
 
 component Syndrome1Calc is
+	port( 
+		R  : in std_logic_vector(254 downto 0); 
+		output : out std_logic_vector(7 downto 0)
+	);
+end component;
+
+component Syndrome3Calc is
 	port( 
 		R  : in std_logic_vector(254 downto 0); 
 		output : out std_logic_vector(7 downto 0)
@@ -23,10 +31,17 @@ begin
 test <= "111000110010101000000110100001100100000010000111110110111110000110110010101011100010001110000010010001100000000101101110000101000111010001110100011101010100001011100101101111001000101010001111101010100011011100011000101100001010111110110100100101110011011";
 --01111011
 
-DUT : Syndrome1Calc
+DUT1 : Syndrome1Calc
 	port map(
 		R  => test,
 		output => S1result
 	);
 
+DUT2 : Syndrome3Calc
+	port map(
+		R  => test,
+		output => S3result
+	);
+
 end architecture;
+
