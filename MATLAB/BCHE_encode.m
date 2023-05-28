2fb7c24c9ff1108d981a2af183ad1d6ee87053ae
Gmaniko <niko220302@gmail.com>
2023-05-29 01:09:55 +0200

    Update BCHE_encode.m

--- MATLAB/BCHE_encode.m
+++ MATLAB/BCHE_encode.m
@@ -1,12 +1,15 @@
 function C = BCHE_encode(X)
+% Encodes information sequence X with the extended BCH(256,239) code.
 
 % Encode with BCH(255,239) code
 C = BCH_encode(X);
 
-% Add even parity bit
+% Calculate even parity bit
 if mod(sum(C),2) == 0
     p = 0;
 else
     p = 1;
 end
+
+% Append parity bit
 C = [C, p];
