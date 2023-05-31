470de6493d01e7969ec531c3fbc1766cfce305c4
Mads <mads@zoie.dk>
2023-05-31 14:46:38 +0200

    synch files

--- VHDL/Synch.vhd
+++ VHDL/Synch.vhd
@@ -5,45 +5,118 @@ use IEEE.std_logic_unsigned.all;
 
 entity Synch is
 	port( 
-		input  : in std_logic_vector(524 downto 0); --takes two inputs at a time
+		input  : in std_logic;
+		clock  : in std_logic;
 		output : out std_logic_vector(254 downto 0)
 	);
 end entity;
 
 architecture Synch_arch of Synch is
 
---Start Pattern:  01100101
-signal PatternMatchCnt,start : integer;
-signal head1,tail2 : integer; --head look for start pattern
-signal tempmsg1, tempmsg2part1, tempmsg2part2 : std_logic_vector(254 downto 0);
+--Start Pattern:  0110 0101
+signal sipo : std_logic_vector(254 downto 0);
+signal sipohead : integer;
+signal state : integer;
+
 
 begin
 
-MatchPattern : process(input)
+PatternMatchState : process(clock,input)
 begin
-	if (input(head1 downto head1-7) = "01100101") then
-		PatternMatchCnt <= PatternMatchCnt + 1; --Found Match
-		tempmsg1 <= input(head1-8 downto head1-8-254); --prep to output foundmsg
-		if (input(head1-8-254-1 downto head1-8-254-8) = "01100101") then --Found first part of second msg
-			tempmsg2part1(head1-8-254-9 downto 0) <= input(head1-8-254-9 downto 0); --prep second msg
-			tail2 <= 524-(524-(head1-8-254-9));
-		end if;
-		tempmsg2part2(524 downto tail2) <= input(524 downto tail2);
-	else
-		PatternMatchCnt <= 0; -- No Match
-		if (head1 > 254+8) then --Move head
-			head1 <= head1-1;
+if (clock'event and clock='1') then
+
+	if(state < 0 or state > 8) then
+		state <= 0;
+	end if;
+
+	if (state = 0) then
+		if(input = '0') then
+			state <= state+1;
 		else
-			head1 <= 524;
+			state <= 0;
 		end if;
 	end if;
+	
+	if (state = 1) then
+		if(input = '1') then
+			state <= state+1;
+		else
+			state <= 0;
+		end if;
+	end if;
+	
+	if (state = 2) then
+		if(input = '1') then
+			state <= state+1;
+		else
+			state <= 0;
+		end if;
+	end if;
+	
+	if (state = 3) then
+		if(input = '0') then
+			state <= state+1;
+		else
+			state <= 0;
+		end if;
+	end if;
+	
+	if (state = 4) then
+		if(input = '0') then
+			state <= state+1;
+		else
+			state <= 0;
+		end if;
+	end if;
+	
+	if (state = 5) then
+		if(input = '1') then
+			state <= state+1;
+		else
+			state <= 0;
+		end if;
+	end if;
+	
+	if (state = 6) then
+		if(input = '0') then
+			state <= state+1;
+		else
+			state <= 0;
+		end if;
+	end if;
+	
+	if (state = 7) then
+		if(input = '1') then
+			state <= state+1;
+		else
+			state <= 0;
+		end if;
+	end if;
+	
+	if (state = 8 and sipohead = -1) then
+		state <= 0;
+	end if;
+
+end if;
 end process;
 
-outputter : process -- så skal den output 2 for hver input
+
+SIPOprocess : process(clock,input)
 begin
-output <= tempmsg2part1 xor tempmsg2part2;
-wait for 1 ns;
-output <= tempmsg1;
+
+if (clock'event and clock='1' and state = 7) then
+	sipohead <= 254;
+end if;
+
+if (clock'event and clock='1' and state = 8) then
+	if (sipohead = -1) then
+		output <= sipo;
+	else
+		sipo(sipohead) <= input;
+		sipohead <= sipohead -1;
+	end if;
+end if;
+
 end process;
 
 end architecture;
